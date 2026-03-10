20cebd83fb3e817a899556e070054ace77792169
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-03-10 23:11:38 +0530

    Initial commit

---
+++ app/components/TypewriterBox.js
@@ -0,0 +1,40 @@
+"use client";
+
+import {useState, useEffect} from "react";
+
+export default function Typewriter({
+    text="",
+    height="200px",
+    width="100%",
+    speed=50,
+    fontSize="1rem",
+    className=""
+}){
+    const [displayedText, setDisplayedText] = useState("");
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        if(index >= text.length) return;
+
+        const timeout = setTimeout(() => {
+            setDisplayedText((prev) => prev + text.charAt(index));
+            setIndex((prev) => prev + 1);
+        }, speed);
+
+        return ()=> clearTimeout(timeout)
+    }, [index, text, speed]);
+
+    useEffect(() => {
+        setDisplayedText("");
+        setIndex(0);
+    }, [text]);
+
+    return(
+        <div className={`relative overflow-hidden border border-slate-700 bg-slate-900 p-4 font-mono text-sm leading-relaxed ${className}`} style={{width, height}}>
+            <div className="h-full overflow-y-auto break-words whitespace-pre-wrap text-emerald-300">
+                {displayedText}
+                <spa className="inline-block w-2 h-4 ml-1 translate-y-1 bg-emerald-300 animate-pulse" />
+            </div>
+        </div>
+    )
+}
